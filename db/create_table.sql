30975d459bd5fae9e378a8b1dc0abb9543c8dfc8
Satish-Masa <kakao2964@gmail.com>
2020-07-17 17:21:21 +0900

    update create_table.sql

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,6 +1,22 @@
 -- name: create-users-table
 CREATE TABLE users (
-    id       INTEGER PRIMARY KEY UNIQUE NOT NULL,
-    email    VARCHAR(255),
-    password VARCHAR(255)
+    id       INTEGER PRIMARY KEY,
+    email    VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+-- name: create-items-table
+CREATE TABLE items (
+    id          INTEGER PRIMARY KEY UNIQUE,
+    name        VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    price       INTEGER NOT NULL
+);
+
+-- name: create-basket-table
+CREATE TABLE basket (
+    item_id INTEGER UNIQUE NOT NULL,
+    user_id INTEGER NOT NULL,
+    name    VARCHAR(255) NOT NULL,
+    price   INTEGER NOT NULL
 );
