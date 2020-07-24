285d50d9015d5ec21db1c2273087b209f75d0b07
Satish-Masa <kakao2964@gmail.com>
2020-07-24 19:55:29 +0900

    update create_table.sql

--- db/create_table.sql
+++ db/create_table.sql
@@ -1,22 +1,31 @@
--- name: create-users-table
+-- +migrate Up
 CREATE TABLE users (
-    id       INTEGER PRIMARY KEY,
+    id       INTEGER NOT NULL AUTO_INCREMENT,
     email    VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL
+    password VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
 );
 
--- name: create-items-table
+-- +migrate Up
 CREATE TABLE items (
-    id          INTEGER PRIMARY KEY,
+    id          INTEGER NOT NULL AUTO_INCREMENT,
     name        VARCHAR(255) NOT NULL,
     description VARCHAR(255),
-    price       INTEGER NOT NULL
+    price       INTEGER NOT NULL,
+    PRIMARY KEY (id)
 );
 
--- name: create-basket-table
+-- +migrate UP
 CREATE TABLE basket (
+    id      INTEGER NOT NULL AUTO_INCREMENT,
     item_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     name    VARCHAR(255) NOT NULL,
-    price   INTEGER NOT NULL
+    price   INTEGER NOT NULL,
+    PRIMARY KEY (id)
 );
+
+-- +migrate Down
+DROP TABLE users;
+DROP TABLE items;
+DROP TABLE basket;
