5677c9560ece46dc29e38b38943bb3a98651b70b
Satish-Masa <kakao2964@gmail.com>
2020-07-26 22:29:23 +0900

    update app layer item.go

--- application/item/item.go
+++ application/item/item.go
@@ -6,6 +6,14 @@ type ItemApplication struct {
 	Repository domainItem.ItemRepository
 }
 
+type ItemRequest struct {
+	ID string `json: "id"`
+}
+
 func (a ItemApplication) GetItemList() ([]domainItem.Item, error) {
 	return a.Repository.Get()
+}
+
+func (a ItemApplication) FindItem(id int) (domainItem.Item, error) {
+	return a.Repository.Find(id)
 }
