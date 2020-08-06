1dcaf978745b8a4842dcc6dc76dfbbafaf8cab41
Satish-Masa <kakao2964@gmail.com>
2020-08-06 13:51:51 +0900

    update app layer auth.go

--- application/auth/auth.go
+++ application/auth/auth.go
@@ -1,7 +1,6 @@
 package auth
 
 import (
-	"net/http"
 	"time"
 
 	domainUser "github.com/Satish-Masa/ec-backend/domain/user"
@@ -10,27 +9,20 @@ import (
 	"github.com/labstack/echo/middleware"
 )
 
+var secret = "2FMd5FNSqS/nW2wWJy5S3ppjSHhUnLt8HuwBkTD6HqfPfBBDlykwLA=="
+
 type jwtCustomClaims struct {
-	UID  int    `json: "uid"`
-	Name string `json: "name"`
+	ID    int    `json:"id"`
+	Email string `json:"email"`
 	jwt.StandardClaims
 }
 
-var signingKey = []byte("secret")
-
 var Config = middleware.JWTConfig{
 	Claims:     &jwtCustomClaims{},
-	SigningKey: signingKey,
+	SigningKey: []byte(secret),
 }
 
 func createToken(u *domainUser.User) (string, error) {
-	if u.Email == "" {
-		return "", &echo.HTTPError{
-			Code:    http.StatusUnauthorized,
-			Message: "invalid name",
-		}
-	}
-
 	claims := &jwtCustomClaims{
 		u.ID,
 		u.Email,
@@ -41,15 +33,12 @@ func createToken(u *domainUser.User) (string, error) {
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
-	t, err := token.SignedString(signingKey)
+	tokenString, err := token.SignedString([]byte(secret))
 	if err != nil {
-		return "", &echo.HTTPError{
-			Code:    http.StatusInternalServerError,
-			Message: "failed to create the token",
-		}
+		return "", err
 	}
 
-	return t, nil
+	return tokenString, nil
 }
 
 func FetchToken(u *domainUser.User) (resp string, err error) {
@@ -61,9 +50,11 @@ func FetchToken(u *domainUser.User) (resp string, err error) {
 	return resp, nil
 }
 
-func FindUserID(c echo.Context) int {
-	user := c.Get("user").(*jwt.Token)
-	claims := user.Claims.(*jwtCustomClaims)
-	uid := claims.UID
-	return uid
+func Check(c echo.Context) domainUser.User {
+	var user domainUser.User
+	u := c.Get("user").(*jwt.Token)
+	claims := u.Claims.(*jwtCustomClaims)
+	user.ID = claims.ID
+	user.Email = claims.Email
+	return user
 }
