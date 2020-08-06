75d3e0b7575c7dcbb876e05d4acbc5675783e633
Satish-Masa <kakao2964@gmail.com>
2020-08-06 13:53:11 +0900

    update rest.go

--- interfaces/rest.go
+++ interfaces/rest.go
@@ -7,10 +7,12 @@ import (
 	"strconv"
 
 	UserAuth "github.com/Satish-Masa/ec-backend/application/auth"
+	AppCart "github.com/Satish-Masa/ec-backend/application/cart"
 	passhash "github.com/Satish-Masa/ec-backend/application/hash"
 	AppItem "github.com/Satish-Masa/ec-backend/application/item"
 	AppUser "github.com/Satish-Masa/ec-backend/application/user"
 	"github.com/Satish-Masa/ec-backend/config"
+	domainCart "github.com/Satish-Masa/ec-backend/domain/cart"
 	domainItem "github.com/Satish-Masa/ec-backend/domain/item"
 	domainUser "github.com/Satish-Masa/ec-backend/domain/user"
 	"github.com/labstack/echo"
@@ -20,6 +22,7 @@ import (
 type Rest struct {
 	UserRepository domainUser.UserRepository
 	ItemRepository domainItem.ItemRepository
+	CartRepository domainCart.CartRepository
 }
 
 func (r Rest) signupHandler(c echo.Context) error {
@@ -139,6 +142,63 @@ func (r Rest) getItemHandler(c echo.Context) error {
 	return c.JSON(http.StatusOK, resp)
 }
 
+func (r Rest) addCartHandler(c echo.Context) error {
+	user := UserAuth.Check(c)
+
+	item := new(domainItem.Item)
+	req := new(AppItem.ItemRequest)
+	if err := c.Bind(req); err != nil {
+		return err
+	}
+
+	item.ID, _ = strconv.Atoi(req.ID)
+
+	capp := AppCart.CartRepository{
+		Repository: r.CartRepository,
+	}
+
+	err := capp.AddCart(*item, user)
+	if err != nil {
+		return &echo.HTTPError{
+			Code:    http.StatusInternalServerError,
+			Message: "Failed to add cart",
+		}
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"Create": "OK",
+	})
+}
+
+func (r Rest) CartListHandler(c echo.Context) error {
+	user := UserAuth.Check(c)
+
+	application := AppCart.CartRepository{
+		Repository: r.CartRepository,
+	}
+
+	carts, err := application.GetCart(user.ID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, nil)
+	}
+
+	iapplication := AppItem.ItemApplication{
+		Repository: r.ItemRepository,
+	}
+	var resp []domainItem.Item
+	for _, cart := range carts {
+		item, err := iapplication.FindItem(cart.ItemID)
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, nil)
+		}
+		resp = append(resp, item)
+	}
+
+	log.Println(resp)
+
+	return c.JSON(http.StatusOK, resp)
+}
+
 func (r Rest) Start() {
 	e := echo.New()
 
@@ -153,5 +213,8 @@ func (r Rest) Start() {
 
 	auth := e.Group("/auth")
 	auth.Use(middleware.JWTWithConfig(UserAuth.Config))
+	auth.POST("/item/add", r.addCartHandler)
+	auth.POST("/cart", r.CartListHandler)
+
 	e.Logger.Fatal(e.Start(fmt.Sprintf(":%d", config.Config.Port)))
 }
