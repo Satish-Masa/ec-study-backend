5351df071cd1ec96fb829c6f67e2b911c06ae5e8
Satish-Masa <kakao2964@gmail.com>
2020-08-11 19:06:44 +0900

    update app layer mail.go

--- application/mail/mail.go
+++ application/mail/mail.go
@@ -1,131 +1,28 @@
 package mail
 
 import (
-	"context"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
-	"net/url"
-	"os"
-	"os/user"
-	"path/filepath"
-	"strings"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/gmail/v1"
+	"github.com/Satish-Masa/ec-backend/config"
+	"github.com/sendgrid/sendgrid-go"
+	"github.com/sendgrid/sendgrid-go/helpers/mail"
 )
 
-// getClient uses a Context and Config to retrieve a Token
-// then generate a Client. It returns the generated Client.
-func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
-	cacheFile, err := tokenCacheFile()
-	if err != nil {
-		log.Fatalf("Unable to get path to cached credential file. %v", err)
-	}
-	tok, err := tokenFromFile(cacheFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(cacheFile, tok)
-	}
-	return config.Client(ctx, tok)
-}
-
-// getTokenFromWeb uses Config to request a Token.
-// It returns the retrieved Token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
-	}
-
-	tok, err := config.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
-	}
-	return tok
-}
-
-// tokenCacheFile generates credential file path/filename.
-// It returns the generated credential path/filename.
-func tokenCacheFile() (string, error) {
-	usr, err := user.Current()
-	if err != nil {
-		return "", err
-	}
-	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
-	os.MkdirAll(tokenCacheDir, 0700)
-	return filepath.Join(tokenCacheDir,
-		url.QueryEscape("gmail-go-quickstart.json")), err
-}
-
-// tokenFromFile retrieves a Token from a given file path.
-// It returns the retrieved Token and any read error encountered.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	t := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(t)
-	defer f.Close()
-	return t, err
-}
-
-// saveToken uses a file path to create a file and store the
-// token in it.
-func saveToken(file string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", file)
-	f, err := os.Create(file)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-func SendMail(email string) error {
-	ctx := context.Background()
-
-	b, err := ioutil.ReadFile("credentials.json")
+func SendEmail(email string) error {
+	from := mail.NewEmail("Check Email", "email@example.com")
+	subject := "Check your Email"
+	to := mail.NewEmail("Check Your Email", email)
+	plainTextContent := "This is login from!! "
+	htmlContent := "<strong>and easy to do anywhere, even with Go</strong>"
+	message := mail.NewSingleEmail(from, subject, to, plainTextContent, htmlContent)
+	client := sendgrid.NewSendClient(config.Config.APIKey)
+	response, err := client.Send(message)
 	if err != nil {
 		return err
-	}
-
-	config, err := google.ConfigFromJSON(b, gmail.MailGoogleComScope)
-	if err != nil {
-		return err
-	}
-	client := getClient(ctx, config)
-
-	srv, err := gmail.New(client)
-	if err != nil {
-		return err
-	}
-
-	var message gmail.Message
-
-	temp := []byte("From: 'me'\r\n" +
-		"reply-to: to@example.com\r\n" +
-		"To:  " + email + "\r\n" +
-		"Subject: Check Your Email \r\n" +
-		"\r\n")
-
-	message.Raw = base64.StdEncoding.EncodeToString(temp)
-	message.Raw = strings.Replace(message.Raw, "/", "_", -1)
-	message.Raw = strings.Replace(message.Raw, "+", "-", -1)
-	message.Raw = strings.Replace(message.Raw, "=", "", -1)
-
-	_, err = srv.Users.Messages.Send("me", &message).Do()
-	if err != nil {
-		return err
+	} else {
+		log.Println(response.StatusCode)
+		log.Println(response.Body)
+		log.Println(response.Headers)
 	}
 	return nil
 }
