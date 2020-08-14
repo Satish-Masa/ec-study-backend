7c1bd0f89fcfb8e0da7988f69e94288bf2ad834a
Satish-Masa <kakao2964@gmail.com>
2020-08-15 00:06:30 +0900

    update database

---
+++ db/z_create_ordered_item.sql
@@ -0,0 +1,12 @@
+-- +migrate Up
+CREATE TABLE ordered {
+    item_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    number  INTEGER NOT NULL DEFAULT 1,
+    FOREIGN KEY (item_id)
+        REFERENCES items(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (user_id)
+        REFERENCES users(id)
+        ON DELETE CASCADE
+}
