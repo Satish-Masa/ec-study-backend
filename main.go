c22dd193c1c005221b5dc712f85af080645546ac
Satish-Masa <kakao2964@gmail.com>
2020-08-15 00:06:14 +0900

    update main.go

--- main.go
+++ main.go
@@ -37,7 +37,6 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer migrate.Exec(db.DB(), driver, migrations, migrate.Down)
 	defer db.Close()
 
 	jpfaker := domainItem.Item{Name: "吾輩は猫である", Description: "吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を", Price: 600, Stock: 100}
@@ -57,10 +56,12 @@ func main() {
 	user := infrastructure.NewUserRepository(db)
 	item := infrastructure.NewItemRepository(db)
 	cart := infrastructure.NewCartRepository(db)
+	ordered := infrastructure.NewOrderedRepository(db)
 	rest := &interfaces.Rest{
-		UserRepository: user,
-		ItemRepository: item,
-		CartRepository: cart,
+		UserRepository:  user,
+		ItemRepository:  item,
+		CartRepository:  cart,
+		OrderRepository: ordered,
 	}
 	rest.Start()
 }
