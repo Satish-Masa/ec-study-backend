e9daa7d5b26ec7bc8793355cc2d33d456f0db72d
Satish-Masa <kakao2964@gmail.com>
2020-08-18 20:06:58 +0900

    update app layer cart.go

--- application/cart/cart.go
+++ application/cart/cart.go
@@ -9,11 +9,16 @@ type CartRepository struct {
 }
 
 type CartResponce struct {
+	ID          int    `json: "id"`
 	Name        string `json: "name"`
 	Description string `json: "description"`
 	Price       int    `json: "price"`
 	Stock       int    `json: "stock"`
 	Number      int    `json: "number"`
+}
+
+type CartDeleteRequest struct {
+	ID int `json: "id"`
 }
 
 func (a CartRepository) AddCart(iid, uid, num int) error {
@@ -22,4 +27,8 @@ func (a CartRepository) AddCart(iid, uid, num int) error {
 
 func (a CartRepository) GetCart(uid int) ([]cart.Cart, error) {
 	return a.Repository.Get(uid)
+}
+
+func (a CartRepository) DeleteCart(uid, iid int) error {
+	return a.Repository.Delete(uid, iid)
 }
