66cbf3971330a339a73c5ab98b4021c4dc39d092
Satish-Masa <kakao2964@gmail.com>
2020-08-24 17:11:43 +0900

    update app layer ordered.go

--- application/ordered/ordered.go
+++ application/ordered/ordered.go
@@ -1,18 +1,22 @@
 package ordered
 
-import "github.com/Satish-Masa/ec-backend/domain/ordered"
+import (
+	"time"
+
+	"github.com/Satish-Masa/ec-backend/domain/ordered"
+)
 
 type OrderedRepository struct {
 	Repository ordered.OrderedRepository
 }
 
 type OrderedResponce struct {
-	ID          int    `json: "id"`
-	Name        string `json: "name"`
-	Description string `json: "description"`
-	Price       int    `json: "price"`
-	Stock       int    `json: "stock"`
-	Number      int    `json: "number"`
+	ID          int       `json: "id"`
+	Created_at  time.Time `json: "created_at`
+	Name        string    `json: "name"`
+	Description string    `json: "description"`
+	Price       int       `json: "price"`
+	Number      int       `json: "number"`
 }
 
 func (a OrderedRepository) AddOrdered(iid, uid, num int) error {
